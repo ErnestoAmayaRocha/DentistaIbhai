04508164a0e104a15304cbf11d83b2b41180bc4b
Luis Valenzuela <luisrav14@hotmail.com>
2023-02-02 21:14:00 -0600

    Roles menú

--- admin/components/sidebar.php
+++ admin/components/sidebar.php
@@ -1,57 +1,61 @@
 <link rel="stylesheet" href="//cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
 
 <nav id="sidebar" class="sidebar js-sidebar">
-    <div class="sidebar-content js-simplebar">
-        <a class="sidebar-brand" href="index.php">
-            <img src="../assets/img/logo2.png" width="80%" alt="logo">
-        </a>
 
-        <ul class="sidebar-nav">
-            <li class="sidebar-header">
-                Informacion general
-            </li>
+    <?php
 
-            <!--   <li class="sidebar-item">
+    if ($_SESSION['rol'] === 'admin') { ?>
+        <div class="sidebar-content js-simplebar">
+            <a class="sidebar-brand" href="index.php">
+                <img src="../assets/img/logo2.png" width="80%" alt="logo">
+            </a>
+
+            <ul class="sidebar-nav">
+                <li class="sidebar-header">
+                    Informacion general
+                </li>
+
+                <!--   <li class="sidebar-item">
                 <a class="sidebar-link" href="bitacora.php">
                     <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Bitácora digital</span>
                 </a>
             </li> -->
-            <li class="sidebar-item">
-                <a class="sidebar-link" href="Ortodoncia.php">
-                    <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Ortodoncia</span>
-                </a>
-            </li>
-            <li class="sidebar-item">
-                <a class="sidebar-link" href="doctores.php">
-                    <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Doctores</span>
-                </a>
-            </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="Ortodoncia.php">
+                        <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Ortodoncia</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="doctores.php">
+                        <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Doctores</span>
+                    </a>
+                </li>
 
-            <li class="sidebar-header">
-                Operaciones
-            </li>
-            <li class="sidebar-item">
-                <a class="sidebar-link" href="noticias.php">
-                    <i class="align-middle" data-feather="tablet"></i> <span class="align-middle">Noticias</span>
-                </a>
-            </li>
+                <li class="sidebar-header">
+                    Operaciones
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="noticias.php">
+                        <i class="align-middle" data-feather="tablet"></i> <span class="align-middle">Noticias</span>
+                    </a>
+                </li>
 
-            <li class="sidebar-item">
-                <a class="sidebar-link" href="calendario.php">
-                    <i class="align-middle" data-feather="tablet"></i> <span class="align-middle">Calendario</span>
-                </a>
-            </li>
-            <li class="sidebar-item">
-                <a class="sidebar-link" href="odontograma.php">
-                    <i class="align-middle" data-feather="tablet"></i> <span class="align-middle">Odontograma</span>
-                </a>
-            </li>
-            <li class="sidebar-item">
-                <a class="sidebar-link" href="examenes-clinicos.php">
-                    <i class="align-middle" data-feather="tablet"></i> <span class="align-middle">Examenes Clínicos</span>
-                </a>
-            </li>
-            <!-- <li class="sidebar-item">
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="calendario.php">
+                        <i class="align-middle" data-feather="tablet"></i> <span class="align-middle">Calendario</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="odontograma.php">
+                        <i class="align-middle" data-feather="tablet"></i> <span class="align-middle">Odontograma</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="examenes-clinicos.php">
+                        <i class="align-middle" data-feather="tablet"></i> <span class="align-middle">Examenes Clínicos</span>
+                    </a>
+                </li>
+                <!-- <li class="sidebar-item">
                 <a data-bs-target="#multi" data-bs-toggle="collapse" class="sidebar-link" aria-expanded="true">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-corner-right-down align-middle">
                         <polyline points="10 15 15 20 20 15"></polyline>
@@ -91,7 +95,50 @@
                     </li>
                 </ul>
             </li> -->
-        </ul>
+            </ul>
 
-    </div>
+        </div>
+    <?php
+    } elseif ($_SESSION['rol'] === 'caja') { ?>
+        <div class="sidebar-content js-simplebar">
+            <a class="sidebar-brand" href="index.php">
+                <img src="../assets/img/logo2.png" width="80%" alt="logo">
+            </a>
+            <ul class="sidebar-nav">
+                <li class="sidebar-header">
+                    Usuario en caja
+                    <hr>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="calendario.php">
+                        <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Agenda</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="doctores.php">
+                        <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Doctores</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="pacientes.php">
+                        <i class="align-middle" data-feather="tablet"></i> <span class="align-middle">Pacientes</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="#">
+                        <i class="align-middle" data-feather="tablet"></i> <span class="align-middle">Caja</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="#">
+                        <i class="align-middle" data-feather="tablet"></i> <span class="align-middle">Mensaje</span>
+                    </a>
+                </li>
+            </ul>
+
+        </div>
+    <?php
+    }
+    ?>
 </nav>
